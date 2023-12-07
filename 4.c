4290309a88de4b6f337cff8c26ebf5f82759e4c5
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2023-12-07 17:13:05 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+/*
+Q.4 Write a Program to print the below pattern using nested for loop.
+1 0 1 0 1
+   1 0 1 0
+      1 0 1
+         1 0
+            1
+
+
+
+*/
+
 main()
 {  int i,j,k;
      for(i=5;i>=1;i--){
