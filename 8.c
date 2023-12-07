d048a8f9c49efdd50552e5a2d69015a83586d454
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2023-12-07 17:14:39 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 
+/*
+
+Q.8 Write a Program to print the below pattern using nested for loop.
+
+81
+81 82 
+81 82 83 
+81 82 83 84 
+81 82 83 84 85
+
+*/
+
 main()
 {  int i,j,k=81;
      for(i=5;i>=1;i--){  
